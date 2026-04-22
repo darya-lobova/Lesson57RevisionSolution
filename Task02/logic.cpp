313978b651d631e8a0b53f05c46f38f33a427afe
st <st@A510PC3>
2026-04-22 17:11:36 +0300

    Task02 solved

--- Task02/logic.cpp
+++ Task02/logic.cpp
@@ -5,3 +5,56 @@
 // Необходимо разработать функцию (или программу), 
 // которая вычисляет сумму элементов матрицы, 
 // расположенных на главной и побочной диагоналях.
+
+#include "logic.h"
+
+int find_sum_of_main_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+	int sum_main = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (i == j) {
+				sum_main += matrix[i][j];
+			}
+		}
+	}
+
+	return sum_main;
+}
+
+int find_sum_of_addition_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+	int sum_addition = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (j == n - 1 - i) {
+				sum_addition += matrix[i][j];
+			}
+		}
+	}
+
+	return sum_addition;
+}
+
+int sum_main_and_second_diagonales_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n) {
+
+	if (n <= 0) {
+		return -1;
+	}
+
+	int mid = 0;
+
+	int result = find_sum_of_main_elements(matrix, n) + find_sum_of_addition_elements(matrix, n);
+
+	if (n % 2 == 1) {
+		mid = n / 2;
+		result -= matrix[mid][mid];
+	}
+
+	return result;
+}
+	
--- Task02/logic.h
+++ Task02/logic.h
@@ -1,3 +1,5 @@
 ﻿#define DEFAULT_SIZE 100
 
 int sum_main_and_second_diagonales_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
+int find_sum_of_main_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
+int find_sum_of_addition_elements(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n);
