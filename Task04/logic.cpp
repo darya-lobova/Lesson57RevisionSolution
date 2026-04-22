59c5318df65688b482147b461ab7553fdf10da8b
st <st@A510PC3>
2026-04-22 17:48:40 +0300

    Task04 developing

--- Task04/logic.cpp
+++ Task04/logic.cpp
@@ -5,3 +5,57 @@
 // Необходимо разработать функцию (или программу), которая 
 // высчитывает сумму элементов в тех столбцах заданной матрицы,
 // которые содержат хотя бы один экстремальный элемент.
+
+#include "logic.h"
+int find_max_element(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n) {
+	int max = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			if (matrix[i][j] > max) {
+				max = matrix[i][j];
+			}
+		}
+	}
+
+	return max;
+}
+
+int find_min_element(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n) {
+	int min = find_max_element(matrix, n);
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			
+		}
+
+	}
+
+	return min;
+
+}
+int sum_elements_of_columns_with_extreme_value(int matrix[DEFAUL_SIZE][DEFAUL_SIZE], int n, int m) {
+
+	if (n <= 0 || m <= 0) {
+		return -1;
+	}
+
+	int max = find_max_element(matrix, n);
+	int min = find_min_element(matrix, n);
+	int sum = 0;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			if (max == matrix[i][max]) {
+				sum += matrix[i][j];
+			}
+		}
+	}
+	return sum;
+}
