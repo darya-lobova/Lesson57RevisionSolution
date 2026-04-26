27d34247e57c2361c3b3f2af5900a90e072a8a36
darya-loboca <dasha.lobova.it@gmail.com>
2026-04-26 18:34:31 +0300

    TaskX solved

--- TaskX/logic.cpp
+++ TaskX/logic.cpp
@@ -1,4 +1,4 @@
-﻿﻿// Task X [The all local minimum]
+﻿// Task X [The all local minimum]
 // Все локальные минимумы
 // 
 // Дана математическая прямоугольная матрица размером N на M.
@@ -9,29 +9,41 @@
 
 string get_all_local_minimums(int matrix[DEFAULT_SIZE][DEFAULT_SIZE], int n, int m) {
 	string s = "";
+	if (n <= 0 || m <= 0) {
+		s += "Error.";
+	}
+	bool first = true;
+
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
 			int current = matrix[i][j];
-			bool is_max = true;
+			bool is_min = true;
 
-			if (i > 0 && matrix[i - 1][j] < current) {
-				is_max = false;
+
+			if (i > 0 && matrix[i - 1][j] <= current) {
+				is_min = false;
 			}
 
-			if (is_max && i < n - 1 && matrix[i + 1][j] < current) {
-				is_max = false;
+			if (is_min && i < n - 1 && matrix[i + 1][j] <= current) {
+				is_min = false;
 			}
 
-			if (is_max && j > 0 && matrix[i][j - 1] < current) {
-				is_max = false;
+			if (is_min && j > 0 && matrix[i][j - 1] <= current) {
+				is_min = false;
 			}
 
-			if (is_max && j < m - 1 && matrix[i][j + 1] < current) {
-				is_max = false;
+			if (is_min && j < m - 1 && matrix[i][j + 1] <= current) {
+				is_min = false;
 			}
 
-			if (is_max) {
-				s += to_string(current) + " ";
+			if (is_min) {
+
+				if (!first) {
+					s += " ";
+				}
+
+				s += to_string(current);
+				first = false;
 			}
 		}
 	}
